2c81540160a11ef2066319ae6abb306586283ef2
iqblfrdsyh <iqblfrdsyh@gmail.com>
2024-11-02 19:28:43 +0700

    update text

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div className="text-center h-screen flex justify-center items-center">
       <h1>
-        <code className="text-blue-600">iqbalferdiansyah-code.vercel</code> <br /> is still under
-        development!
+        <code className="text-blue-600">iqbalferdiansyah-code.vercel.app</code>{" "}
+        <br /> is still under development!
       </h1>
     </div>
   );
